aba40fc03bdd21e4efe3d8e693c99328f658b537
Xinan <youkingan11@gmail.com>
2023-08-30 15:28:47 +0100

    add emailJS function

---
+++ app.js
@@ -0,0 +1,40 @@
+// Navbar toggle code (remains the same)
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+// Initialize EmailJS
+emailjs.init("nceDrwlcTso4jndD5");
+
+// Add event listener to form
+window.onload = function () {
+  const form = document.getElementById("contact-form");
+  const status = document.getElementById("status");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Remove previous status classes if any
+    status.classList.remove("success", "error");
+
+    // Service ID and Template ID from EmailJS
+    const SERVICE_ID = "service_3fmhs9i";
+    const TEMPLATE_ID = "template_ual8bpc";
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form).then(
+      function () {
+        form.reset();
+        status.classList.add("success");
+        status.innerHTML = "Thanks! I'll get back as soon as I can.";
+      },
+      function (error) {
+        status.classList.add("error");
+        status.innerHTML = "Oops! There was an error.";
+      }
+    );
+  });
+};
