09b5332ae7225165703b693cf4649c8eac3d661e
Xinan <youkingan11@gmail.com>
2023-08-30 16:26:55 +0100

    add the scroll function

--- app.js
+++ app.js
@@ -38,3 +38,68 @@ window.onload = function () {
     );
   });
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to handle scrolling to a specific section
+  function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  }
+
+  // Check for query parameters on page load
+  const urlParams = new URLSearchParams(window.location.search);
+  const section = urlParams.get("section");
+  if (section) {
+    scrollToSection(section);
+  }
+
+  // For Home link
+  const homeLink = document.querySelector('.navbar__item a[href="index.html"]');
+  homeLink.addEventListener("click", function (event) {
+    if (
+      window.location.pathname === "/index.html" ||
+      window.location.pathname === "/"
+    ) {
+      event.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  });
+
+  // For About and Contact sections
+  const aboutLink = document.querySelector('.navbar__item a[href="#aboutme"]');
+  const contactLink = document.querySelector(
+    '.navbar__item a[href="#contactme__form"]'
+  );
+
+  aboutLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (
+      window.location.pathname !== "/index.html" &&
+      window.location.pathname !== "/"
+    ) {
+      window.location.href = "/index.html?section=aboutme";
+    } else {
+      scrollToSection("aboutme");
+    }
+  });
+
+  contactLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (
+      window.location.pathname !== "/index.html" &&
+      window.location.pathname !== "/"
+    ) {
+      window.location.href = "/index.html?section=contactme__form";
+    } else {
+      scrollToSection("contactme__form");
+    }
+  });
+});
